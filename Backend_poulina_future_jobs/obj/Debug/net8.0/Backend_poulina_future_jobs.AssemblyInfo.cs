d7a8abc325e783d7056d6baf9456c825a447919f
moulaimen <votre_email>
2025-03-01 15:05:59 +0100

    integrate dashboard not done etles message tostr

--- Backend_poulina_future_jobs/obj/Debug/net8.0/Backend_poulina_future_jobs.AssemblyInfo.cs
+++ Backend_poulina_future_jobs/obj/Debug/net8.0/Backend_poulina_future_jobs.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend_poulina_future_jobs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56087dcb8285b5cbb46f86ff8eed7e965177240a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e1dfaba4672ee65856cb8491c5d0fa490f5e92b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend_poulina_future_jobs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend_poulina_future_jobs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
